eba79cbcc57e60bd63fc6fbd920b6ff72ce87f82
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-07-13 21:28:22 -0300

    Add files via upload

---
+++ Davi_130721.py
@@ -0,0 +1,25 @@
+Programar=False
+print(Programar)
+#Question3
+print("3*****************************")
+for item in range(1,11):
+    if item > 8:
+        print(True or False)
+       
+#Question4
+print("4*****************************")
+if Programar:
+   print("There was at least one number bigger than 8")
+else:
+    print("There were no numbers bigger than 8")
+
+#Question5
+print("5*****************************")
+for item in range(1,13):
+    if item < 4:
+        print(item)
+    if item > 4 and item < 8:
+        print(item)
+    
+
+
